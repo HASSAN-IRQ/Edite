6b75bd4a03d03f7c73c119db0ebb7b0eb5b5df9e
HASSAN-IRQ <irq.hassan@yahoo.com>
2016-10-24 14:27:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
-define('API_KEY','token');
-$admin = "216990494";
+define('API_KEY','273410412:AAGjxa4M_DK5LA1l9I0kZwPPRvT7zmxXy4E');
+$admin = "3167020";
 function bot($method,$datas=[]){
     $url = "https://api.telegram.org/bot".API_KEY."/".$method;
     $ch = curl_init();
